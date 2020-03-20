252b24fc99f62ac1d33634b19fab553a9e1208ec
envq-laptop <enrico.sgarbanti@gmail.com>
2020-03-20 21:15:50 +0100

    update with photores test

--- src/function.cpp
+++ src/function.cpp
@@ -10,17 +10,39 @@
     R2 = R1 * V2 / (V1 - V2)
 */
 
-const double MIDDLE_ADC = BIT_ADC / 2.0;
+const double MIDDLE_ADC = MAX_VOLT / 2.0;
+double inc = 0;
 
-int count = 0;
-bool inv = true;
-int compare_photores(const double ADC_VAL) {
-    if (ADC_VAL == MIDDLE_ADC)
-        return 0;
-    else if (ADC_VAL > MIDDLE_ADC)
-        return 1;
-    else
-        return -1;
+int compare_photores(const double ADC_VAL, const double EPSILON,
+                     const bool DEBUG) {
+    double ADC_VOLT = ADC_VAL * VOLTS_PER_BIT;
+    int res;
+    if (ADC_VOLT < MIDDLE_ADC) {
+        if (ADC_VOLT > MIDDLE_ADC - EPSILON) {
+            res = 0;
+        } else {
+            res = -1;
+        }
+    } else {
+        if (ADC_VOLT < MIDDLE_ADC + EPSILON) {
+            res = 0;
+        } else {
+            res = 1;
+        }
+    }
+
+    if (DEBUG) {
+        Serial.print("VOLT: ");
+        Serial.println(ADC_VOLT);
+        Serial.print("EPS+: ");
+        Serial.println((MIDDLE_ADC + EPSILON));
+        Serial.print("EPS-: ");
+        Serial.println((MIDDLE_ADC - EPSILON));
+        Serial.print("RES: ");
+        Serial.println(res);
+        Serial.println("------");
+    }
+    return res;
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,16 @@ const int MOTOR_IN3 = 10;  // PHASEA2 - C
 const int MOTOR_IN4 = 11;  // PHASEB2 - D
 
 // Config
+const double EPSILON_RES = 0.10;
 const int DELAY_TRACKER = 3000;
 const int DELAY_MOTOR_IN = 5;
-const bool DEBUG = true;
+const bool DEBUG_RES = false;
+const bool DEBUG_MOTOR = false;
 
 // Variables
 double ADC_val = 0.0;
 int direction = 0;
-StepperMotor motor(MOTOR_IN1, MOTOR_IN2, MOTOR_IN3, MOTOR_IN4, DEBUG);
+StepperMotor motor(MOTOR_IN1, MOTOR_IN2, MOTOR_IN3, MOTOR_IN4, DEBUG_MOTOR);
 
 
 void setup() {
@@ -28,11 +30,8 @@ void setup() {
 }
 
 void loop() {
-    // Read the ADC value
-    ADC_val = analogRead(PIN_PHOTORES);
-
-    // Get direction from photoresistences comparing
-    while ((direction = compare_photores(ADC_val)) != 0) {
+    while ((direction = compare_photores(analogRead(PIN_PHOTORES), EPSILON_RES,
+                                         DEBUG_RES)) != 0) {
         if (direction == 1)
             motor.moveCW();
         else
